7f18e16e191029ecf492e03d017653ef822a439c
michael huggins <michaelhuggins@protonmail.com>
2020-11-27 08:14:26 -0800

    finally seem to have added exception handling in make list of treee locations.  exception handling in R is quite stupid,

--- make_list_of _tree_locations.r
+++ make_list_of _tree_locations.r
@@ -8,53 +8,47 @@ library(geoNEON)
 
 options(stringsAsFactors=F) ##This is redundant for R versions 4.0.0+
 
-sites <- c('BART',
-           'HARV',
-           'BLAN',
-           'SCBI',
-           'SERC',
-           'DSNY',
-           'JERC',
-           'OSBS',
-           'GUAN',
-           'LAJA',
-           'STEI',
-           'TREE',
-           'UNDE',
-           'KONA',
-           'KONZ',
-           'UKFS',
-           'GRSM',
-           'MLBS',
-           'ORNL',
-           'DELA',
-           'LENO',
-           'TALL',
-           'DCFS',
-           'NOGP',
-           'WOOD',
-           'CPER',
-           'RMNP',
-           'STER',
-           'CLBJ',
-           'OAES',
-           'YELL',
-           'MOAB',
-           'NIWO',
-           'JORN',
-           'SRER',
-           'ONAQ',
-           'ABBY',
-           'WREF',
-           'SJER',
-           'SOAP',
-           'TEAK',
-           'BARR',
-           'TOOL',
-           'BONA',
-           'DEJU',
-           'HEAL',
-           'PUUM')
+sites <-  c('BART',
+            'HARV',
+            'SCBI',
+            'SERC',
+            'DSNY',
+            'JERC',
+            'OSBS',
+            'GUAN',
+            'LAJA',
+            'STEI',
+            'TREE',
+            'UNDE',
+            'KONZ',
+            'UKFS',
+            'GRSM',
+            'MLBS',
+            'ORNL',
+            'DELA',
+            'LENO',
+            'TALL',
+            'DCFS',
+            'NOGP',
+            'WOOD',
+            'CPER',
+            'RMNP',
+            'CLBJ',
+            'YELL',
+            'MOAB',
+            'NIWO',
+            'JORN',
+            'SRER',
+            'ONAQ',
+            'ABBY',
+            'WREF',
+            'SJER',
+            'SOAP',
+            'TEAK',
+            'BONA',
+            'DEJU',
+            'HEAL',
+            'PUUM')
 notfirst <- FALSE
 
 go <- function(notfirst, sites){
@@ -65,11 +59,14 @@ go <- function(notfirst, sites){
   # Merge tables to join tree-specific data with the geospatial data
   veg <- merge(vst_apparentindividual, vegmap, by=c("individualID","namedLocation","domainID","siteID","plotID"))
   write.table(veg, "exact_locations_within_plots.csv", append=notfirst, sep=",", col.names=!notfirst, row.names=FALSE, quote=FALSE)
-  print(notfirst)
   notfirst <- TRUE
   return(notfirst)
 }
 
 for (site in sites) {
-  notfirst <- go(notfirst, sites)
+  result = tryCatch({
+  notfirst <- go(notfirst, sites) 
+  }, 
+  error = function(){
+  })
 }
